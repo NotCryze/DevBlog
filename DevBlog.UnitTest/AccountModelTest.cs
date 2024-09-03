0e62c454a1979d1206d541a0cc387d3d73798adc
Sammy Schröder <samm0413@elevcampus.dk>
2024-09-03 10:42:40 +0200

    Updated unit tests

---
+++ DevBlog.UnitTest/AccountModelTest.cs
@@ -0,0 +1,51 @@
+using DevBlog.Domain.Models;
+
+namespace DevBlog.UnitTest
+{
+    public class AccountModelTest
+    {
+        [Fact(DisplayName = "Create Account")]
+        public void Create()
+        {
+            // Arrange
+            string firstName = "John";
+            string lastName = "Doe";
+            string email = "johndoe@example.com";
+            string password = "P@ssw0rd";
+            string expected = "John Doe";
+
+            // Act
+            Account account = new Account(firstName, lastName, email, password);
+
+            // Assert
+            Assert.Equal(firstName, account.FirstName);
+            Assert.Equal(lastName, account.LastName);
+            Assert.Equal(expected, account.FullName);
+            Assert.Equal(email, account.Email);
+            Assert.Equal(password, account.Password);
+            Assert.False(account.IsAdmin);
+        }
+
+        [Fact(DisplayName = "Create Admin Account")]
+        public void CreateAdmin()
+        {
+            // Arrange
+            string firstName = "John";
+            string lastName = "Doe";
+            string email = "johndoe@example.com";
+            string password = "P@ssw0rd";
+            bool isAdmin = true;
+
+            // Act
+            Account account = new Account(firstName, lastName, email, password, isAdmin);
+
+            // Assert
+            Assert.Equal(firstName, account.FirstName);
+            Assert.Equal(lastName, account.LastName);
+            Assert.Equal(firstName + " " + lastName, account.FullName);
+            Assert.Equal(email, account.Email);
+            Assert.Equal(password, account.Password);
+            Assert.True(account.IsAdmin);
+        }
+    }
+}
