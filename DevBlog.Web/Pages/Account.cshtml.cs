d3ba5bb729aaebece0349ed5021d9b722da6e044
Sammy <samm0413@elevcampus.dk>
2024-10-03 16:04:35 +0200

    Added simple account page that displays a users firstname, lastname and email. Also added input fields for when there will be a change password option. Theres no option to edit anything yet.

--- DevBlog.Web/Pages/Account.cshtml.cs
+++ DevBlog.Web/Pages/Account.cshtml.cs
@@ -1,3 +1,6 @@
+using DevBlog.Domain.IRepo;
+using DevBlog.Domain.Models;
+using DevBlog.Web.DTO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +8,16 @@ namespace DevBlog.Web.Pages
 {
     public class AccountModel : PageModel
     {
+        private readonly IAccountRepository _accountService;
+        public AccountModel(IAccountRepository accountService)
+        {
+            _accountService = accountService;
+        }
+
+        public Account Account { get; set; }
         public void OnGet()
         {
+            Account = _accountService.GetAccount(((AccountDTO)HttpContext.Items["User"]).Id);
         }
     }
 }
