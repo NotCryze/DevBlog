2435921c14d5dfa4c41c49de493ae27c959db3c9
Sammy Schröder <samm0413@elevcampus.dk>
2024-10-09 14:02:08 +0200

    Added ConnectionString to app.config file and added the nessecary sql nugget packages. Started on the blogpost repo and added a sql class to handle the creation of sqlcommands.

---
+++ DevBlog.Domain/Repositories/BlogPostRepository.cs
@@ -0,0 +1,23 @@
+﻿using DevBlog.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace DevBlog.Domain.Repositories
+{
+    public class BlogPostRepository
+    {
+        private SQL _sql;
+        public BlogPostRepository()
+        {
+            _sql = new SQL();
+        }
+
+        public bool CreateBlogPost(BlogPost blogPost)
+        {
+            SqlCommand cmd = _sql.ExecuteSP("spCreateBlogPost");
+
+            return false;
+        }
+    }
+}
---
+++ DevBlog.Domain/SQL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DevBlog.Domain
+{
+    internal class SQL
+    {
+        private readonly string _connectionString;
+
+        /// <summary>
+        /// Executes SQL Stored Procedures
+        /// </summary>
+        /// <param name="storedProcedure"></param>
+        /// <returns>SqlCommand</returns>
+        public SqlCommand Execute(string storedProcedure)
+        {
+            SqlCommand sqlCommand = new SqlCommand(storedProcedure, new SqlConnection(_connectionString));
+            sqlCommand.CommandType = CommandType.Text;
+            return sqlCommand;
+        }
+
+        public SqlCommand ExecuteSP(string storedProcedure)
+        {
+            SqlCommand sqlCommand = new SqlCommand(storedProcedure, new SqlConnection(_connectionString));
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+            return sqlCommand;
+        }
+
+        public SQL()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["DevBlogCon"].ConnectionString;
+        }
+
+    }
+}
